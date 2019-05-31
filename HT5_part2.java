eaf087e05771ed94d015f54eccb5c40a88524816
kachel9 <kachelya@gmail.com>
2019-05-31 20:52:22 +0300

    add 4th task

--- HT5_part2.java
+++ HT5_part2.java
@@ -1,38 +1,40 @@
 public class HT5_part2 {
-    public static void main(String[] args){
-        Point num1 = new Point(1,2);
+    public static void main(String[] args) {
+        Point num1 = new Point(1, 2);
         num1.info();
-        Point num2 = new Point(3,5);
+        Point num2 = new Point(3, 5);
         num2.info();
-        Point num3 = new Point(0,0);
+        Point num3 = new Point(7, 6);
         num3.info();
-        Point num4 = new Point(5,9);
+        Point num4 = new Point(5, 9);
         num4.info();
 
-        Point[]one = new Point[4];
-        one[0]=num1;
-        one[1]=num2;
-        one[2]=num3;
-        one[3]=num4;
+        Point[] one = new Point[4];
+        one[0] = num1;
+        one[1] = num2;
+        one[2] = num3;
+        one[3] = num4;
         for (int j = 0; j < one.length - 1; j++) {
-            one[j].info();}
-
-        /*
-        int [] arr=one[j].info;
+          //  one[j].info();
+        }
         for (int j = 0; j < one.length - 1; j++) {
-            if (one[j]%2==0)
-                System.out.print(j+"");*/
-            }
-             }
+            one[j].sumPoint();
+        }
+    }
+}
 class Point {
     int x;
     int y;
     void info(){
-        System.out.println(x+y);
+   //     System.out.println(x+y);
+    }
+    void sumPoint() {
+        if (0!=(x + y) % 2) {
+            System.out.println(x + y);
+        }
     }
     Point(int x, int y){
         this.x=x;
         this.y=y;
-
+        }
     }
-}
